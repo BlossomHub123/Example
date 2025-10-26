0d740de118fe19d26375bbdffd15a6909e10e0cb
Blossom Hub <oren.sincock@gmail.com>
2025-10-26 14:05:49 +1300

    Create .lua

---
+++ .lua
@@ -0,0 +1,35 @@
+Webhook = "Your Webhook Here" -- Put nothing in between the "" if you dont wanna use one
+
+Config = {
+    ["Pet"] = {
+        {"All Huges", MaxPrice = "23m", PercentOffRap = "40%", AllowGolden = true, AllowRainbow = true, AllowShiny = true},
+        {"All Titanics", MaxPrice = "1b", PercentOffRap = "30%", AllowGolden = true, AllowRainbow = true, AllowShiny = true},
+        {"All Gargantuans", MaxPrice = "1.3b", PercentOffRap = "30%", AllowGolden = true, AllowRainbow = true, AllowShiny = true}
+    },
+    ["Enchant"] = {
+        {"Active Huge Overload", "Boss Chest Mimic", "Boss Lucky Block", "Breakable Mayhem", "Chest Mimic", "Corruption", "Diamond Chest Mimic", "Diamond Gift Hunter", "Diamond Orb", "Double Coins",
+        "Lighning Orb", "Lucky Block", "Massive Comet", "Mega Chest Breaker", "Nightmare Orb", "Party Time", "Rainbow Egg", "Super Magnet", "Super Shiny Hunter", "Superior Chest Mimic", "Superior Wisdom", MaxPrice = "5b", PercentOffRap = "45%"}
+    },
+    ["Egg"] = {
+        {"Active Huge Egg", "Buff Gym Egg", "Cosmic Basketball Egg", "Dark Unit Egg", "Event Cool Egg", "Exclusive Anime Egg", "Exclusive Atlantean Egg", "Exclusive Aura Egg", "Exclusive Balloon Egg", "Exclusive Beast Egg",
+        "Exclusive Bejeweled Egg", "Exclusive Black Hole Egg", "Exclusive Blazing Egg", "Exclusive Brainrot Egg", "Exclusive Capybara Egg", "Exclusive Cartoon Egg", "Exclusive Chad Egg", "Exclusive Classic Egg",
+        "Exclusive Cosmic Egg", "Exclusive Crystal Egg", "Exclusive Dominus Egg", "Exclusive Dot Matrix Egg", "Exclusive Dragon Egg", "Exclusive Elemental Egg", "Exclusive Emoji Egg", "Exclusive Flex Egg", "Exclusive Fruit Egg",
+        "Exclusive Googly Egg", "Exclusive Hologram Egg", "Exclusive Horror Egg", "Exclusive Hypnotic Egg", "Exclusive Jelly Egg", "Exclusive Jolly Egg", "Exclusive Jurassic Egg", "Exclusive Kaiju Egg", "Exclusive Luchador Egg",
+        "Exclusive Matryoshka Egg", "Exclusive Meme Egg", "Exclusive Midnight Egg", "Exclusive Neon Egg", "Exclusive Neon Twilight Egg", "Exclusive Nightmare Egg", "Exclusive Original Egg", "Exclusive Patchwork Egg", "Exclusive Sad Egg",
+        "Exclusive Sketch Egg", "Exclusive Snow Globe Egg", "Exclusive Snowflake Egg", "Exclusive Soul Egg", "Exclusive Spirit Egg", "Exclusive SpongeBob Egg", "Exclusive Stargazing Egg", "Exclusive Super Coral Egg", "Exclusive Super Jelly Egg",
+        "Exclusive Superhero Egg", "Exclusive Totem Egg", "Exclusive Treasure Egg", "Exclusive Valentine's Egg", "Exclusive Valkyrie Egg", "Exclusive Xmas Egg", "Exclusive Yin-Yang Egg", "Gargantuan Machine Grim Reaper Egg", "Grinch Egg", "Gym Egg",
+        "Huge Machine Angelus Egg", "Huge Machine Blurred Dominus Egg", "Huge Machine Butterfly Egg", "Huge Machine Kraken Egg", "Magma Unit Egg", "Valentine's Present", MaxPrice = "15b", PercentOffRap = "40%"}
+    },
+    ["Lootbox"] = {
+        {"Hype Egg", "Hype Egg#2", "Adventurer's Lootbox", "Global Event Gift", "Halloween Lucky Block Gift", "Titanic Easter Gift", "Time Trial Gift", "2023 Titanic Christmas Present", "2024 Anniversary Gift", "2024 Gargantuan Christmas Present",
+        "2024 New Year's Gift", "2025 New Year's Gift", "Adoption Gift", "Autumn Gift", "Basketball Gift", "Buff Gym Gift", "Candy Corn Gift", "Clan Gift", "Clan Gift #2", "Color Gift", "Cosmic Basketball Gift", "Dino Gift", "Doodle Gift",
+        "Elemental Gift", "Evil Gift", "Ghostface Gift", "Glitched Gift", "Good Gift", "Good vs. Evil Gift", "Graffiti Gift", "Gym Gift", "Hellfire Gift", "Huge Easter Gift", "Jolly Gift", "Love Gift", "Lucky Gift", "Magma Tower Defense Gift",
+        "Matrix Tower Defense Gift", "Meteor Gift", "Pet Games Gift", "Pumpkin Gift", "Rave Gift", "School Gift", "Slime Gift", "Snowflake Gift", "Spider Gift", "Summer Gift", "Sun Angelus Gift", "Tower Defense Gift", "Tropical Gift",
+        "Turkey Gift", "Wooden Gift", MaxPrice = "300m", PercentOffRap = "50%"}
+    },
+    ["Ultimate"] = {
+        {"Black Hole", "Hidden Treasure", "Nightmare", "UFO", "Chest Spell", MaxPrice = "5b", PercentOffRap = "40%"}
+    }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BlossomHub123/Ps99-Booth-Sniper/refs/heads/main/.lua", true))()
